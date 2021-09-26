738e3e7c072e702b2e7f79f96321acd7b36f744c
luoyingmm <1502972236@qq.com>
2021-09-26 14:42:18 +0800

    Web Init

---
+++ blog-api/src/main/java/com/luoyingmm/config/mybatisPlusConfig.java
@@ -0,0 +1,18 @@
+package com.luoyingmm.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.luoyingmm.blog.dao")
+public class mybatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+}
