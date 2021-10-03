4be7188980840cdee61a0fe07d643223e5cc57e5
luoyingmm <1502972236@qq.com>
2021-10-03 18:58:01 +0800

    bug fix

--- blog-api/src/main/java/com/luoyingmm/utils/IPUtils.java
+++ blog-api/src/main/java/com/luoyingmm/utils/IPUtils.java
@@ -1,9 +1,9 @@
 package com.luoyingmm.utils;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 
